4bfde45e468bfe4fdb75302d5138208d0feebc5a
Subhashini Movva <subhashinimovva6@gmail.com>
2026-05-27 21:22:04 +0530

    Add files via upload

---
+++ Day-2/day2.py
@@ -0,0 +1,37 @@
+Python 3.13.13 (tags/v3.13.13:01104ce, Apr  7 2026, 19:25:48) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> MyvarPython 3.13.13 (tags/v3.13.13:01104ce, Apr  7 2026, 19:25:48) [MSC v.1944 64 bit (AMD64)] on win32
+SyntaxError: leading zeros in decimal integer literals are not permitted; use an 0o prefix for octal integers
+>>> Myvar=10
+>>> Myvar1=10
+>>> Myvar@1=10
+SyntaxError: cannot assign to expression here. Maybe you meant '==' instead of '='?
+>>> my_var=10
+>>> my var=10
+SyntaxError: invalid syntax
+>>> myvar=10
+>>> Myvar=20
+>>> myvar
+10
+>>> Myvar
+20
+>>> a=10
+>>> b=5
+>>> a
+10
+>>> b
+5
+>>> a=b=75
+>>> a
+75
+>>> b
+75
+>>> a,b,c=7,5,0
+>>> a
+7
+>>> c
+0
+>>> a,b=7,5
+>>> a,b=b,a
+>>> a
+5
